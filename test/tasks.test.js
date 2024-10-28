23edbf5d4b8ede00f9c58f39bb14c995be4e84f5
Cesar Hernandez <149346749+cesarurielhr@users.noreply.github.com>
2024-10-28 00:19:01 -0700

    Parte 1 de pruebas automatizadas

--- test/tasks.test.js
+++ test/tasks.test.js
@@ -6,29 +6,43 @@ const app = require('../index');
 const taskController = require('../controllers/taskController');
 
 const expect = chai.expect;
-describe('GET /tasks',()=>{
-    it('1. Deberia devolver todas las tareas con estatus 200 cuando hay tareas',async ()=>{
-    const tasks = [
-    {
-        id: 1,
-        title: "Tarea 1",
-        description: "Descripción de la Tarea 1",
-    },
-    {
-        id: 2,
-        title: "Tarea 2",
-        description: "Descripción de la Tarea 2",
-    },
-    {
-        id: 3,
-        title: "Tarea 3",
-        description: "Descripción de la Tarea 3",
-    },
-    ];
-    const res = await request(app).get('/tasks');
-    expect(res.status).to.equal(200);
-    expect(res.body).to.be.an('array');
-    expect(res.body.length).to.equal(3);
-    expect(res.body).to.deep.equal(tasks);
+let token;
+
+//Pruebas para firebases 
+// Como primer paso nos autenticamos para poder realizar la consulta
+    before(async () => {
+        const res = await request(app).post('/auth/login').send({ username: 'admin', password: '12345' });
+        token = res.body.token;
+        expect(token).to.be.a('string');
+    });
+//Muestra todas las tareas 
+describe('GET ALL TASKS /tasks', () => {
+    //Seguidamente con el token valido realizamos la consulta
+    it('7, Deberia devolver todas las tareas con estatus 200 cuando hay tareas en Firebase', async () => {
+        const res = await request(app).get('/tasks').set('Authorization', `Bearer ${token}`);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('array');
+        
+      
+
+    });
+     });
+// Muestra una tarea específica
+describe('GET SPECIFIC TASK /tasks/:id', () => {
+    it('Debería devolver una tarea con estatus 200 cuando la tarea existe en Firebase', async () => {
+        const taskId = 'AkSZQbLCwowI9pycCLOM'; // Reemplaza con un ID real de tarea para la prueba
+        const res = await request(app).get(`/tasks/${taskId}`).set('Authorization', `Bearer ${token}`);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('object');
+   
+    });
 });
-});
+
+
+
+
+
+
+
+
+
