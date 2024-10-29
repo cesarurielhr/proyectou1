2f1a932d29c8794d7a1548fd6a587837868e247e
Cesar Hernandez <149346749+cesarurielhr@users.noreply.github.com>
2024-10-28 17:26:17 -0700

    Pruebas automatizadas Completado

--- test/tasks.test.js
+++ test/tasks.test.js
@@ -3,13 +3,13 @@ const chai = require('chai');
 const sinon = require('sinon');
 
 const app = require('../index');
-const taskController = require('../controllers/taskController');
+
 
 const expect = chai.expect;
 let token;
-
+let taskId2
 //Pruebas para firebases 
-// Como primer paso nos autenticamos para poder realizar la consulta
+// Primero se iniciamos sesion con el usuario que queremos
     before(async () => {
         const res = await request(app).post('/auth/login').send({ username: 'admin', password: '12345' });
         token = res.body.token;
@@ -17,30 +17,75 @@ let token;
     });
 //Muestra todas las tareas 
 describe('GET ALL TASKS /tasks', () => {
-    //Seguidamente con el token valido realizamos la consulta
-    it('7, Deberia devolver todas las tareas con estatus 200 cuando hay tareas en Firebase', async () => {
+    it('1. Deberia devolver todas las tareas con estatus 200 cuando hay tareas en Firebase', async () => {
         const res = await request(app).get('/tasks').set('Authorization', `Bearer ${token}`);
         expect(res.status).to.equal(200);
         expect(res.body).to.be.an('array');
-        
+        console.log(res.body);
       
 
     });
      });
 // Muestra una tarea específica
 describe('GET SPECIFIC TASK /tasks/:id', () => {
-    it('Debería devolver una tarea con estatus 200 cuando la tarea existe en Firebase', async () => {
-        const taskId = 'AkSZQbLCwowI9pycCLOM'; // Reemplaza con un ID real de tarea para la prueba
+    it('2. Debería devolver una tarea con estatus 200 cuando la tarea existe en Firebase', async () => {
+        const taskId = 'AkSZQbLCwowI9pycCLOM'; // ID DE LA TAREA 
         const res = await request(app).get(`/tasks/${taskId}`).set('Authorization', `Bearer ${token}`);
         expect(res.status).to.equal(200);
         expect(res.body).to.be.an('object');
+        console.log(res.body);
    
     });
+
+});
+// Muestra una tarea específica
+describe('POST NEW TASK /tasks', () => {
+    it('3. Debería devolver una tarea con estatus 200 cuando la tarea se haya creado en Firebase', async () => {
+        const res = await request(app).post(`/tasks`).send(
+        {
+        title: "Tarea 5",
+        description: "Descripción de para tarea de arjona",
+        createdAt:"2024-10-29",
+        completed:"false"
+        }
+        ).set('Authorization', `Bearer ${token}`);
+
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('object');
+        console.log(res.body);
+        taskId2 = res.body.id; 
+    });
+    
 });
 
+// Edita una tarea especifica
+describe('PUT EDIT TASK /tasks/:idu', () => {
+    it('4. Debería devolver una tarea con estatus 200 cuando la tarea se haya actualizado en Firebase', async () => {
+         
+        const res = await request(app).put(`/tasks/${taskId2}`).send(
+        {
+        completed:true
+        }
+        ).set('Authorization', `Bearer ${token}`);
 
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('object');
+        console.log(res.body);
+   
+    });
 
+});
 
+// Elimna una tarea en espefico
+describe('DELETE TASKS /tasks/:idd', () => {
+    it('5. Debería devolver una tarea con estatus 200 cuando la tarea se haya eliminado en Firebase', async () => {
+        const res = await request(app).delete(`/tasks/${taskId2}`).set('Authorization', `Bearer ${token}`);
+
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('string');
+        console.log(res.body);     
+    });
+});
 
 
 
