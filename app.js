183cbceb1037a49bb56c80ef31c16a5c8186ef64
JoseLinoDC <joselinodiaz25@gmail.com>
2024-10-29 23:02:12 -0700

    Update Final

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ app.use(bodyParser.json());
 app.use('/tareas', taskRoutes);
 app.use('/auth', authRoutes);
 
-app.get('/', (req, res, next) => {
-    res.send(
-        `<h1>API RESTFULL de TASK con Firebase</h1> <p> Leer: <a href="docs.com">api-tasks-docs</a> para mas información.</p>`
-    );
-})
-
 app.use((req, res, next) => {
     res.status(404).json({ code: 404, message: 'Ruta no encontrada' });
 });
